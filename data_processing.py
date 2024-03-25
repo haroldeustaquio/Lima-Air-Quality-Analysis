7bf423afc32a455bba6d8d2ef52d87ff0d6c4941
Harold Eustaquio <harold.eustaquio.a@uni.pe>
2024-03-25 18:44:39 -0500

    Add files via upload

--- data_processing.py
+++ data_processing.py
@@ -1,15 +1,20 @@
 from funciones import *
 import pandas as pd
+from time import sleep
 
-conn = conect_db()
 
-air_data = pd.read_sql_query("SELECT * FROM air_data", conn)
+while True:
+    conn = conect_db()
 
-clean_data(air_data).to_csv('data/air_data.csv',index=False)
+    air_data = pd.read_sql_query("SELECT * FROM air_data", conn)
 
-# place = pd.read_sql_query("select * from place_data",conn)
-# place['location']= place['location'].apply(lambda x: clean_string(x))
-# place['distrito']= place['distrito'].apply(lambda x: clean_string(x))
-# place.to_csv("data/place.csv",index=False)
+    clean_data(air_data).to_csv('data/air_data.csv',index=False)
 
-print("Hecho")
+    # place = pd.read_sql_query("select * from place_data",conn)
+    # place['location']= place['location'].apply(lambda x: clean_string(x))
+    # place['distrito']= place['distrito'].apply(lambda x: clean_string(x))
+    # place.to_csv("data/place.csv",index=False)
+
+    print("Hecho")
+    sleep
+    sleep(1807)
